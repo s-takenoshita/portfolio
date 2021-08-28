d278c816cddd3c5bde431f020828995927f77f0e
竹之下伸一 <89330246+s-takenoshita@users.noreply.github.com>
2021-08-28 14:53:06 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// headerのイメージをクリックした時
+
+let sw = true;
+console.log(sw);
+
+function process() {
+  if (sw) {
+    document.getElementById('header_text').textContent = '目標：年内に収入を得る！';
+    document.getElementById('takenoko').classList.add('img_changed');
+    sw = false;
+  } else {
+    document.getElementById('header_text').textContent = 'フリーランスを目指し勉強中のアラフィフです。';
+    document.getElementById('takenoko').classList.add('img_changed_back');
+    sw = true;
+  }
+}
+document.getElementById('takenoko').addEventListener('click', process);
