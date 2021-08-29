25bdc90100c0702bd7c8e11875bdd9fd7cd7e751
竹之下伸一 <89330246+s-takenoshita@users.noreply.github.com>
2021-08-29 11:31:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 'use strict';
 
-// headerのイメージをクリックした時
-
-let sw = true;
+// 日付をフッターに表示
 let d = new Date();
 console.log(d);
 console.log(d.getTime());
+document.getElementById('date').textContent = d;
 
-function process() {
-  if (sw) {
-    document.getElementById('header_text').textContent = '目標：年内に収入を得る！';
-    document.getElementById('takenoko').classList.add('img_changed');
-    sw = false;
-  } else {
-    document.getElementById('header_text').textContent = 'フリーランスを目指し勉強中のアラフィフです。';
-    document.getElementById('takenoko').classList.add('img_changed_back');
-    sw = true;
-  }
-}
-document.getElementById('takenoko').addEventListener('click', process);
+// ヘッダーのアイコンの角丸を変える
+document.getElementById('takenoko').addEventListener('click', () => {
+  const target = document.getElementById('takenoko');
+  target.classList.toggle('changeradius');
+});
+
+// 経歴を追加する
+const history = [
+  {yaer: 1985, lang: 'C Unix', desc: '経歴' },
+  {yaer: 1995, lang: 'C++ Macintosh', desc: '経歴' },
+];
+document.querySelector('button').addEventListener('click', () => {
+  const parenttarget1 = document.getElementById('profile');
+  history.forEach(h => {
+    console.log(h.yaer);
+    const tg_yaer = document.createElement('div');
+    tg_yaer.textContent = h.yaer;
+    tg_yaer.className = 'yaer';
+    const tg_histry = document.createElement('div');
+    tg_histry.className = 'history';
+    tg_histry.appendChild(tg_yaer);
+    parenttarget1.appendChild(tg_histry);
+  });
+});
+
+// クリックされたリストに色を付ける
+document.querySelector('ul').addEventListener('click', e => {
+  const lis = document.querySelectorAll('li');
+  console.log(lis);
+  lis.forEach(li => {
+    li.classList.remove('color');
+  });
+  e.target.classList.add('color');
+});
