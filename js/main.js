53be6893de27b0a1b103584247ee455ca9f1fef9
竹之下伸一 <89330246+s-takenoshita@users.noreply.github.com>
2021-09-02 05:59:39 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -40,3 +40,21 @@ document.querySelector('ul').addEventListener('click', e => {
   });
   e.target.classList.add('color');
 });
+
+// モーダルウィンドウ
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal');
+const mask = document.getElementById('mask');
+open.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+  mask.classList.remove('hidden');
+});
+close.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  mask.classList.add('hidden');
+});
+mask.addEventListener('click', () => {
+  close.click();
+});
+
